e85a7c92bf592878f3fd4bb4b3abd00a6a7806ce
David Weissenborn <weissguymail-git@yahoo.com>
2018-05-13 23:32:41 -0500

    Implement Printjobs complete

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -1,6 +1,7 @@
 package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -14,6 +15,7 @@ public class TechJobs {
     public static void main (String[] args) {
 
         // Initialize our field map with key/name pairs
+
         HashMap<String, String> columnChoices = new HashMap<>();
         columnChoices.put("core competency", "Skill");
         columnChoices.put("employer", "Employer");
@@ -111,6 +113,25 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if(someJobs.size() == 0) {
+            System.out.println("Nothing to print");
+        } else {
+
+            for (HashMap job : someJobs) {
+
+                Collection jobValues = job.values();
+                Collection jobKeys = job.keySet();
+
+                System.out.println("*****");
+
+                for (Object jobKey : jobKeys) {
+                    System.out.println(jobKey+" : "+job.get(jobKey));
+                }
+
+            }
+            System.out.println("*****");
+            //          System.out.println(someJobs.size() + " jobs printed ");
+        }
+
     }
 }
